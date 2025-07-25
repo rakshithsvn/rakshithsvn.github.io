a03bbd4959c866fa21e2b87efcee5f487d6879ae
rakshithsvn <rakshithsvn@gmail.com>
2025-07-25 17:01:35 +0530

    deploy path changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-developer-portfolio/',
+  base: '/rakshithsvn.github.io/',
   plugins: [react()],
 })
